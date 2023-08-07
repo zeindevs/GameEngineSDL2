e20c85648ebf7e3ad9fa1e1295e4bd9c8a3abe9d
zeindevs <zeindevs@gmail.com>
2023-08-07 15:15:42 +0700

    add: limiting frame rate

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,28 @@ Game* game;
 
 int main(int argv, char *args[])
 {
+	const int FPS = 30;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
 	game = new Game();
 	game->init("GameEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 640, false);
 
 	while (game->running()) {
+
+		frameStart = SDL_GetTicks();
+
 		game->handleEvents();
 		game->update();
 		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime) {
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->cleanUp();
